3702a6451802a9328504027883a3f79b7c6ec17e
Ademene <51475097+Ademene@users.noreply.github.com>
2021-03-19 18:39:48 +0100

    Add files via upload

---
+++ 02_Manual_curation_of_the_new_reference_genome/Calcul_coverage_bam.sh
@@ -0,0 +1,4 @@
+### Ne marche pas en bash... à copier dans le terminal
+
+bamCoverage --bam ESM015_ILL_ESM015_3.sorted.bam -of bedgraph -o ESM015_ILL_ESM015_3.sorted.coverage.bedgraph -bs 10000 --normalizeUsing RPGC --effectiveGenomeSize 43305322 -ignore ESM_Mitochondria -p 18
+
